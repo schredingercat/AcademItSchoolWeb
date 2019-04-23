46df14d2d31e3002254262b7cc3585cdd208a200
Ilya Bogomolov <bogomolov_comfortlab@bitbucket.org>
2019-04-23 18:26:07 +0700

    Создание обработчика нажатия на кнопку

--- AcademItSchoolWeb/TempConverter/convert.js
+++ AcademItSchoolWeb/TempConverter/convert.js
@@ -1,3 +1,16 @@
 "use strict";
 
+function ready() {
+    var button = document.querySelector(".convert-button");
+
+    var convertTemperature = function (e) {
+        alert(e);
+    };
+
+    button.addEventListener("click", convertTemperature);
+}
+document.addEventListener("DOMContentLoaded", ready);
+
+
+
 console.log("Hello World!");
