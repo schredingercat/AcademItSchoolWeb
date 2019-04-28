ca3d3dc72d9ca75ad3d63b024e1c72e315c56e3c
Богомолов Илья <schredinger@mail.ru>
2019-04-28 22:16:13 +0700

    Реализована функция пересчета температуры

--- AcademItSchoolWeb/TempConverter/convert.js
+++ AcademItSchoolWeb/TempConverter/convert.js
@@ -1,10 +1,27 @@
 "use strict";
 
 function ready() {
-    var button = document.querySelector(".convert-button");
+    var button = document.getElementById("convertButton");
 
     var convertTemperature = function (e) {
-        alert(e);
+        var result = document.getElementById("result");
+
+        var input = document.getElementById("tempTextInput");
+        var celsiusValue = parseFloat(input.value);
+        console.log(celsiusValue);
+
+        if (isNaN(celsiusValue)) {
+            result.innerText = "Неверный ввод";
+            return;
+        }
+
+        if (celsiusValue < -273.15) {
+            result.innerText = "Температура не может быть ниже -273.15°С";
+            return;
+        }
+
+        result.innerText = celsiusValue + "°С = " + (celsiusValue + 273.15) + "K = " + (1.8 * celsiusValue + 32) + "°F";
+
     };
 
     button.addEventListener("click", convertTemperature);
