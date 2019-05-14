c20fb16bf9b65867c22283fac9df72204078172d
Ilya Bogomolov <bogomolov_comfortlab@bitbucket.org>
2019-05-14 18:45:24 +0700

    Добавлены GUID для записей

--- AcademItSchoolWeb/ToDoList/todo-script.js
+++ AcademItSchoolWeb/ToDoList/todo-script.js
@@ -6,9 +6,10 @@ $(function () {
 
         var input = $(".input-text");
         if (input.val().trim().length !== 0) {
-            var insert = $("<div class='row justify-content-between pl-3 pr-3 pt-1 pb-1'><div>" + input.val() + "</div>"
+            var guid = getNewGuid();
+            var insert = $("<div class='row justify-content-between pl-3 pr-3 pt-1 pb-1'><div id='" + guid + "'>" + input.val() + "</div>"
                 + "<div class='btn-group' role='group'>"
-                + "<button type='button' class='btn btn-outline-primary'><i class='fas fa-edit'></i></button>"
+                + "<button type='button' class='btn btn-outline-primary' data-toggle='modal' data-target='.bd-example-modal-lg' data-uniqueid='" + guid + "'><i class='fas fa-edit'></i></button>"
                 + "<button type='button' class='btn btn-outline-danger removeButton'><i class='fas fa-trash-alt'></i></button>"
                 + "</div></div>");
             $(".todo-list").append(insert);
@@ -21,11 +22,20 @@ $(function () {
         input.val("");
     });
 
-
-
+    $(".bd-example-modal-lg").on("show.bs.modal", function (e) {
+        var elementId = e.relatedTarget.dataset.uniqueid;
+        alert(elementId);
+    });
 
 });
 
 var removeNote = function (note) {
     note.remove();
+};
+
+var getNewGuid = function () {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+    return s4() + s4() + "-" + s4() + "-" + s4() + "-" + s4() + "-" + s4() + s4() + s4();
 };
