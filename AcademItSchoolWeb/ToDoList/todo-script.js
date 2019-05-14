8bc3a69e1d74aafda348ad595bfcafdc2993ac6c
Ilya Bogomolov <bogomolov_comfortlab@bitbucket.org>
2019-05-14 15:36:06 +0700

    Добавлена кнопка удаления записи

--- AcademItSchoolWeb/ToDoList/todo-script.js
+++ AcademItSchoolWeb/ToDoList/todo-script.js
@@ -6,10 +6,22 @@ $(function () {
 
         var input = $(".input-text");
         if (input.val().trim().length !== 0) {
-            var insert = $("<div class='row'><div>" + input.val() + "</div></div>");
+            var insert = $("<div>" + input.val() + "<button type='button' class='btn btn-outline-danger m-1 removeButton'><img src='./images/x.svg'/></button></div>");
             $(".todo-list").append(insert);
+
+            $(".removeButton").on("click", function (e) {
+                var note = $(this).parent();
+                removeNote(note);
+            });
         }
         input.val("");
     });
 
+
+
+
 });
+
+var removeNote = function (note) {
+    note.remove();
+};
