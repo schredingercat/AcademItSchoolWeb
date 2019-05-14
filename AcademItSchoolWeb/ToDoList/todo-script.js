8ae0f7db06a2cbb6a92823ed36af7bbea753948f
Ilya Bogomolov <bogomolov_comfortlab@bitbucket.org>
2019-05-14 14:40:34 +0700

    Добавлена валидация ввода

---
+++ AcademItSchoolWeb/ToDoList/todo-script.js
@@ -0,0 +1,15 @@
+﻿"use strict";
+
+$(function () {
+
+    $(".add-button").on("click", function (e) {
+
+        var input = $(".input-text");
+        if (input.val().trim().length !== 0) {
+            var insert = $("<div class='row'><div>" + input.val() + "</div></div>");
+            $(".todo-list").append(insert);
+        }
+        input.val("");
+    });
+
+});
