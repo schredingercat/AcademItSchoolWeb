6f977f86a0f4b5fc21adfd6f1e164640e3d36322
Ilya Bogomolov <bogomolov_comfortlab@bitbucket.org>
2019-05-14 17:41:24 +0700

    Кнопкам добавлены иконки

--- AcademItSchoolWeb/ToDoList/todo-script.js
+++ AcademItSchoolWeb/ToDoList/todo-script.js
@@ -6,11 +6,15 @@ $(function () {
 
         var input = $(".input-text");
         if (input.val().trim().length !== 0) {
-            var insert = $("<div>" + input.val() + "<button type='button' class='btn btn-outline-danger m-1 removeButton'><img src='./images/x.svg'/></button></div>");
+            var insert = $("<div class='row justify-content-between pl-3 pr-3 pt-1 pb-1'><div>" + input.val() + "</div>"
+                + "<div class='btn-group' role='group'>"
+                + "<button type='button' class='btn btn-outline-primary'><i class='fas fa-edit'></i></button>"
+                + "<button type='button' class='btn btn-outline-danger removeButton'><i class='fas fa-trash-alt'></i></button>"
+                + "</div></div>");
             $(".todo-list").append(insert);
 
             $(".removeButton").on("click", function (e) {
-                var note = $(this).parent();
+                var note = $(this).parent().parent();
                 removeNote(note);
             });
         }
